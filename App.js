f2a9c0bbb9c0c4c0b3b37f8ae54e3e2e2159c0fd
Matheus <matheus.granda@gmail.com>
2022-03-28 07:38:40 -0300

    Commit do projeto

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native'
+import MainScreen from './src/components/MainScreen'
+
+const App = () => {
+  return (
+    <NavigationContainer>
+
+      <MainScreen></MainScreen>
+
+    </NavigationContainer>
+
+  );
+};
+
+
+
+export default App;
